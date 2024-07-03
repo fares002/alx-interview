eae34e5af3ec8ae24ba920f6cc0dd1e905b32b31
fares002 <fares@example.com>
2024-07-03 01:13:50 +0000

    done

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+from math import factorial
+
+
+def pascal(rows):
+    """
+    function thats print pascal tringle with ncr law
+    """
+    triangle = []
+    for n in range(rows):
+        row = []
+        for r in range(n+1):
+            ncr = factorial(n) // (factorial(r) * factorial(n-r))
+            row.append(ncr)
+        triangle.append(row)
+    return triangle
