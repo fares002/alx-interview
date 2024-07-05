a5cf659634dc239f4c798bd56ace9a9a2aa6afeb
fares002 <fares@example.com>
2024-07-05 16:25:20 +0000

    done

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -17,6 +17,9 @@ def pascal_triangle(rows):
     [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
     """
 
+    if rows <= 0:
+        return []
+
     triangle = [[1]]
     for i in range(rows - 1):
         temp = [0] + triangle[-1] + [0]
