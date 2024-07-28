b54ab5dbb1ef8461644f7c8ff5073c831429b10b
fares002 <fares@example.com>
2024-07-28 01:45:15 +0000

    done

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,39 +4,46 @@ Module to determine if a given data set represents a valid UTF-8 encoding.
 """
 
 def validUTF8(data):
-    """Determines if a given data set represents a valid UTF-8 encoding"""
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
     
-    def get_binary_representation(num):
-        """Returns the 8-bit binary representation of a number."""
-        return format(num, '08b')
-
+    :param data: List[int] - list of integers representing bytes
+    :return: bool - True if data is a valid UTF-8 encoding, else False
+    """
+    
     n_bytes = 0
 
-    for num in data:
-        bin_rep = get_binary_representation(num)
+    # Masks to check the most significant bits (MSB)
+    mask1 = 1 << 7  # 10000000
+    mask2 = 1 << 6  # 01000000
 
+    for num in data:
+        # Check if the number is within the byte range
+        if num > 255:
+            return False
+
+        # If this is the start of a new UTF-8 character
         if n_bytes == 0:
-            # Determine the number of bytes in the UTF-8 character
-            if bin_rep[0] == '0':
-                # 1-byte character (0xxxxxxx)
+            # Count the number of leading 1's in the first byte
+            mask = 1 << 7
+            while mask & num:
+                n_bytes += 1
+                mask >>= 1
+
+            # If no leading 1's, it's a single-byte character
+            if n_bytes == 0:
                 continue
-            elif bin_rep[:3] == '110':
-                # 2-byte character (110xxxxx)
-                n_bytes = 1
-            elif bin_rep[:4] == '1110':
-                # 3-byte character (1110xxxx)
-                n_bytes = 2
-            elif bin_rep[:5] == '11110':
-                # 4-byte character (11110xxx)
-                n_bytes = 3
-            else:
-                # Invalid first byte pattern for UTF-8
+
+            # UTF-8 characters can be 1 to 4 bytes long
+            if n_bytes == 1 or n_bytes > 4:
                 return False
+
         else:
-            # Check that the byte starts with '10' (10xxxxxx)
-            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
+            # Check that the byte starts with '10'
+            if not (num & mask1 and not (num & mask2)):
                 return False
-            n_bytes -= 1
+
+        n_bytes -= 1
 
     # All characters should be complete (n_bytes should be 0)
     return n_bytes == 0
