381440ec84ffe3114ca30070d7a021ca567aa605
fares002 <fares@example.com>
2024-08-11 23:54:57 +0000

    task2

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,41 +2,26 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
-const baseUrl = 'https://swapi-api.alx-tools.com/api';
-
-function fetchCharacter(url) {
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(JSON.parse(body).name);
-      }
-    });
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
+    }
   });
-}
+};
 
-async function getAndPrintCharacters() {
-  try {
-    const movieUrl = `${baseUrl}/films/${movieId}/`;
-    const movieData = await new Promise((resolve, reject) => {
-      request(movieUrl, (error, response, body) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(JSON.parse(body));
-        }
-      });
-    });
-
-    const characterPromises = movieData.characters.map(fetchCharacter);
-    const characters = await Promise.all(characterPromises);
-
-    characters.forEach(character => console.log(character));
-  } catch (error) {
-    console.error('An error occurred:', error);
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
+    }
   }
-}
-
-getAndPrintCharacters();
+);
