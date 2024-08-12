8caca31565363e05d6741f47b4eaf08ae00cffc9
fares002 <fares@example.com>
2024-08-12 00:01:28 +0000

    task2

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,36 @@
 #!/usr/bin/node
 /**
- * A script that prints all characters of a Star Wars movie
-*/
+ * A script that prints all characters of a Star Wars movie.
+ * The Movie ID is provided as a positional argument.
+ */
 const request = require('request');
-const filmNum = process.argv[2] + '/';
-const filmURL = 'https://swapi-api.alx-tools.com/api/films/';
-request(filmURL + filmNum, async (err, res, body) => {
+const filmId = process.argv[2];  // Movie ID from command line argument
+const filmURL = `https://swapi-api.alx-tools.com/api/films/${filmId}/`;  // URL to fetch film data
+
+// Fetch film data
+request(filmURL, async (err, res, body) => {
   if (err) {
     console.error(err);
     return;
   }
-  const charactersURLList = JSON.parse(body).characters;
-  for (const characterURL of charactersURLList) {
-    await new Promise((resolve, reject) => {
-      request(characterURL, (err, res, body) => {
-        if (err) {
-          console.error(err);
-          reject(err);
-          return;
-        }
-        console.log(JSON.parse(body).name);
-        resolve();
+  try {
+    const charactersURLList = JSON.parse(body).characters;  // Get characters URLs
+    for (const characterURL of charactersURLList) {
+      await new Promise((resolve, reject) => {
+        // Fetch each character data
+        request(characterURL, (err, res, body) => {
+          if (err) {
+            console.error(err);
+            reject(err);
+            return;
+          }
+          console.log(JSON.parse(body).name);  // Print character name
+          resolve();
+        });
       });
-    });
+    }
+  } catch (e) {
+    console.error('Failed to parse JSON:', e);
   }
 });
+
