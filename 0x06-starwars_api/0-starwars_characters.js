de1148443f790b5007bebf4b9e62aa2728b3f1fc
fares002 <fares@example.com>
2024-08-12 00:19:39 +0000

    task2

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,60 +1,41 @@
 #!/usr/bin/node
-/**
- * A script that prints all characters of a Star Wars movie.
- * The Movie ID is provided as a positional argument.
- */
 const request = require('request');
 
 // Get the movie ID from the command line arguments
 const movieId = process.argv[2];
 
-// Validate that the movie ID is provided
-if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <movie_id>');
-  process.exit(1);
-}
-
 // Construct the URL for the specified movie
-const filmURL = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 // Fetch movie data
-request(filmURL, (err, res, body) => {
-  if (err) {
-    console.error('Error fetching film data:', err);
-    process.exit(1);
-  }
-
-  let filmData;
-  try {
-    filmData = JSON.parse(body);
-  } catch (e) {
-    console.error('Error parsing film JSON:', e);
-    process.exit(1);
-  }
-
-  const charactersURLList = filmData.characters;
-
-  if (!charactersURLList || charactersURLList.length === 0) {
-    console.log('No characters found for this movie.');
-    return;
-  }
-
-  // Fetch each character data
-  charactersURLList.forEach((characterURL) => {
-    request(characterURL, (err, res, body) => {
-      if (err) {
-        console.error('Error fetching character data:', err);
+request(url, (err, response, body) => {
+    if (err) {
+        console.error(err);
         return;
-      }
+    }
 
-      let characterData;
-      try {
-        characterData = JSON.parse(body);
-        console.log(characterData.name);
-      } catch (e) {
-        console.error('Error parsing character JSON:', e);
-      }
-    });
-  });
+    const movieData = JSON.parse(body);
+    const characters = movieData.characters;
+
+    // Function to fetch a single character's data and return a promise
+    const fetchCharacter = (characterUrl) => {
+        return new Promise((resolve, reject) => {
+            request(characterUrl, (err, response, body) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    const characterData = JSON.parse(body);
+                    resolve(characterData.name);
+                }
+            });
+        });
+    };
+
+    // Fetch all characters and print their names
+    Promise.all(characters.map(fetchCharacter))
+        .then(characterNames => {
+            characterNames.forEach(name => console.log(name));
+        })
+        .catch(err => console.error(err));
 });
 
