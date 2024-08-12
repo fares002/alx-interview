1d04e71b82c5e424d1b4a99eecf39e8bdd361ec8
fares002 <fares@example.com>
2024-08-12 00:00:16 +0000

    task2

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,20 +2,23 @@
 /**
  * A script that prints all characters of a Star Wars movie
 */
-
 const request = require('request');
 const filmNum = process.argv[2] + '/';
 const filmURL = 'https://swapi-api.alx-tools.com/api/films/';
-
 request(filmURL + filmNum, async (err, res, body) => {
-  if (err) return console.error(err);
-
+  if (err) {
+    console.error(err);
+    return;
+  }
   const charactersURLList = JSON.parse(body).characters;
-  for (const chracterURL of charactersURLList) {
+  for (const characterURL of charactersURLList) {
     await new Promise((resolve, reject) => {
-      request(chracterURL, (err, res, body) => {
-        if (err) return console.error(err);
-
+      request(characterURL, (err, res, body) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+          return;
+        }
         console.log(JSON.parse(body).name);
         resolve();
       });
